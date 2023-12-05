04567bea689472f9ef7aa230e0f2efea8dca3348
Calvin K <calvinknaub@gmail.com>
2023-12-04 16:51:12 -0800

    made the delete work

--- api/server.js
+++ api/server.js
@@ -3,6 +3,22 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
+server.put('/api/users/:id', (req, res) => {
+    
+})
+
+server.delete("/api/users/:id", async (req, res) => {
+  const possibleUser = await User.findById(req.params.id);
+  if (!possibleUser) {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist",
+    });
+  } else {
+    const deletedUser = await User.remove(possibleUser.id);
+    res.status(200).json(deletedUser)
+  }
+});
+
 server.post("/api/users", (req, res) => {
   const user = req.body;
   if (!user.name || !user.bio) {
@@ -10,16 +26,16 @@ server.post("/api/users", (req, res) => {
       message: "Please provide name and bio for the user",
     });
   } else {
-      User.insert(user)
-          .then((createdUser) => {
-            res.status(201).json(createdUser);
-        })
-        .catch((err) => {
-          res.status(500).json({
-            message: "error creating user",
-            err: err.message,
-          });
+    User.insert(user)
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
         });
+      });
   }
 });
 
