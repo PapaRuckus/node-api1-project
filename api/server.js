4169595ecc088286a5bb51d9737341c0a2885e50
Calvin K <calvinknaub@gmail.com>
2023-12-04 16:23:46 -0800

    made the server find the user by id

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ const server = express();
 
 server.get("/api/users", (req, res) => {
   User.find()
-      .then(users => {
-        res.json(users)
+    .then((users) => {
+      res.json(users);
     })
     .catch((err) => {
       res.status(500).json({
@@ -18,11 +18,16 @@ server.get("/api/users", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
-      res.json(user)
+      if (!user) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      }
+      res.json(user);
     })
     .catch((err) => {
-      res.status(500).json({
-        message: "error getting users",
+      res.status(404).json({
+        message: "error getting user",
         err: err.message,
       });
     });
