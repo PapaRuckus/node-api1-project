7002f81ffa8271fd7f70e2177b5c0e803659ff59
Calvin K <calvinknaub@gmail.com>
2023-12-04 17:07:07 -0800

    finished

--- api/server.js
+++ api/server.js
@@ -3,7 +3,30 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
-server.put("/api/users/:id", (req, res) => {});
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "Please provide name and bio for the user",
+        });
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body,);
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error updating user",
+      err: err.message,
+    });
+  }
+});
 
 server.delete("/api/users/:id", async (req, res) => {
   try {
@@ -18,7 +41,7 @@ server.delete("/api/users/:id", async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({
-      message: "error creating user",
+      message: "error deleting user",
       err: err.message,
     });
   }
